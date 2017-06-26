71a74bfd2207aaedccad8fff9776005633ad95dd
Ben McAvoy <mcavoybn@gmail.com>
2017-06-26 13:53:16 -0600

    Auth Functionality

--- public/app/auth/controllers/register.ctrl.js
+++ public/app/auth/controllers/register.ctrl.js
@@ -5,9 +5,28 @@
         .module('app')
         .controller('RegisterCtrl', RegisterCtrl);
 
-    RegisterCtrl.$inject = ['$scope', '$state'];
+    RegisterCtrl.$inject = ['$scope', '$state', '$firebaseAuth'];
 
-    function RegisterCtrl($scope, $state) {
-
+    function RegisterCtrl($scope, $state, $firebaseAuth) {
+        $scope.submit = submit;
+        $scope.email;
+        $scope.password;
+        $scope.confirmPassword = '';
+        $scope.message = '';
+        
+        function submit(){
+            if($scope.password == $scope.confirmPassword){
+                $scope.message = '';
+                return $firebaseAuth().$createUserWithEmailAndPassword($scope.email, $scope.password)
+                    .then( (data) => {
+                        $state.go('crews', { id: data.uid } )
+                    })
+                    .catch( (error) => {
+                        $scope.message = error;        
+                    });
+            }else{
+                $scope.message = "Passwords did not match!"
+            }
+        }
     }
 })();

464c99c729264caa298dcccaafc70c9b3095cbd3
Ben McAvoy <mcavoybn@gmail.com>
2017-06-27 11:38:22 -0600

    Git is a blast to use

--- public/app/crews/controllers/crews.ctrl.js
+++ public/app/crews/controllers/crews.ctrl.js
@@ -5,16 +5,29 @@
     .module('app')
     .controller('CrewsCtrl', CrewsCtrl);
     
-    CrewsCtrl.$inject = ['$scope', '$firebaseAuth', '$firebaseArray'];
+    CrewsCtrl.$inject = ['$scope', '$firebaseAuth', '$firebaseArray', 'ModalService'];
     
-    function CrewsCtrl($scope, $firebaseAuth, $firebaseArray) {
-        console.log($firebaseAuth().$getAuth());
-
+    function CrewsCtrl($scope, $firebaseAuth, $firebaseArray, ModalService) {
+        $scope.showCreateCrewForm = showCreateCrewForm;
+        
         activate();
         
         function activate() {
             var crewsRef = firebase.database().ref().child($firebaseAuth().$getAuth().uid).child('crews');
             $scope.crews = $firebaseArray(crewsRef);
+        }
+        
+        function showCreateCrewForm() {
+            ModalService.showModal({
+                templateUrl: 'app/crews/templates/createCrewModal.tpl.html',
+                controller: 'ModalCtrl'
+            }).then((modal) => {
+                modal.element.modal();
+                modal.close.then((result) => {
+                    $('.modal-backdrop').remove();
+                    console.log('Closed ' + result);
+                });
+            });
         }
     }
 })();

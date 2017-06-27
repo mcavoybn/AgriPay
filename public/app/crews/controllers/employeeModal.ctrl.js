0f856714f9d622511a55e692779607f96aa5caac
tyrocoders <zach@tyrocoders.com>
2017-06-27 13:07:03 -0600

    Refactored employee modal

--- public/app/crews/controllers/employeeModal.ctrl.js
+++ public/app/crews/controllers/employeeModal.ctrl.js
@@ -9,21 +9,8 @@
 
     function EmployeeModalCtrl($scope, $state, $stateParams, $firebaseArray, $firebaseAuth, ModalService) {
         $scope.employees;
+        $scope.employee;
         activate();
-
-        $scope.firstName;
-        $scope.lastName;
-        $scope.middle;
-        $scope.SSN;
-        $scope.gender;
-        $scope.dateOfBirth;
-        $scope.address1;
-        $scope.address2;
-        $scope.city;
-        $scope.state;
-        $scope.zipCode;
-        $scope.phoneNumber;
-        $scope.hourlyRate;
 
         function activate(){
             var authObj = $firebaseAuth($stateParams.id);
@@ -45,23 +32,8 @@
         }
 
         function submit(){
-            var newEmployee = {
-                firstName: $scope.firstName,
-                lastName: $scope.lastName,
-                middle: $scope.middle,
-                SSN: $scope.SSN,
-                gender: $scope.gender,
-                dateOfBirth: $scope.dateOfBirth,
-                address1: $scope.address1,
-                address2: $scope.address2,
-                city: $scope.city,
-                state: $scope.state,
-                zipCode: $scope.zipCode,
-                phoneNumber: $scope.phoneNumber,
-                hourlyRate: $scope.hourlyRate
-            }
-            employees.$add(newEmployee);
-            employees.$save();
+            $scope.employees.$add($scope.employee);
+            $scope.employees.$save();
         }
     }
 })();
